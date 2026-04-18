87686024bd2a715db1020778a69d49f185d4effd
Anuj-nagriya <Anujnagriya7@gmail.com>
2026-04-18 12:39:52 +0530

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> stk;
+
+        for(char i : s){
+            if(i == '(' || i == '{' || i == '['){
+                stk.push(i);
+            }else{
+
+                if(stk.empty() == true) return false;
+
+                char top = stk.top();
+                if((top == '(' && i == ')') ||
+                   (top == '[' && i == ']') ||
+                   (top == '{' && i == '}')){
+                        stk.pop();
+                    }else{
+                        return false;
+                    }
+            }
+        }
+        return stk.empty();
+    }
+};
